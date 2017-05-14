73881370927175ca7b3816c70e1d325ab876bfcd
Steven Knox <stevenk32@hotmail.com>
2017-05-14 16:40:51 +0100

    Added missing IEntityWithState to GenericEntity

--- GenericApi/GenericEntity.cs
+++ GenericApi/GenericEntity.cs
@@ -2,7 +2,7 @@
 
 namespace GenericApi
 {
-    public class GenericEntity: IHasGenericRepository
+    public class GenericEntity: IHasGenericRepository, IEntityWithState
     {
         [NotMappedAttribute]
         public EntityState EntityState { get; set; }
